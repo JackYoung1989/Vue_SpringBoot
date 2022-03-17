ab9f4fc4cd895659df61f0d417f444a97e88913e
JackYoung <yangjieseagull@163.com>
2022-03-17 09:19:15 +0800

    完成最基本的表header但是不能显示出效果，待查原因。

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
+    <div style="width: 200px">后台管理</div>
+    <div style="flex: 1"></div>
+    <div style="width: 100px">下拉框</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
