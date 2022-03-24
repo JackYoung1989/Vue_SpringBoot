b7387d85ada11853496e96646147ae971d5db19e
JackYoung <yangjieseagull@163.com>
2022-03-24 18:17:16 +0800

    完成登录注册部分。

---
+++ vue/src/layout/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <MyHeader />
+
+  <div style="display: flex">
+    <Aside />
+    <router-view />
+  </div>
+</div>
+</template>
+
+<script>
+import MyHeader from "../components/MyHeader";
+import Aside from "../components/Aside";
+
+export default {
+  components: {
+    MyHeader,
+    Aside
+  },
+  name: "Layout"
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+<div style="width: 100%; height: 100vh; background-color: darkslateblue; overflow: hidden">
+  <div style="width: 400px; margin: 100px auto">
+    <div style="color: #cccccc; font-size: 30px; text-align: center; padding: 30px">欢迎登录</div>
+    <el-form ref="form" :model="form" size="default">
+      <el-form-item>
+        <el-input prefix-icon="Search" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input prefix-icon="setting" v-model="form.password" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button style="width: 100%" type="primary" @click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</div>
+</template>
+
+<script>
+import request from "../utils/request";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {}
+    }
+  },
+  methods: {
+    login() {
+      request.post("/user/login", this.form).then(res => {
+        if (res.code === "0") {
+          this.$message({
+            type: "success",
+            message: "登录成功"
+          })
+          this.$router.push("/") //登录成功之后进行页面跳转，到首页
+        } else {
+          this.$message({
+            type: "error",
+            message: res.msg
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
