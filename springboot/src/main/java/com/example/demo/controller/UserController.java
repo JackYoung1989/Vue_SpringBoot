ade94203ba39b4da8538b48650ca7425cc215234
JackYoung <yangjieseagull@163.com>
2022-03-24 13:07:47 +0800

    增删改查已经完成，下面开始添加登录注册相关内容。

--- springboot/src/main/java/com/example/demo/controller/UserController.java
+++ springboot/src/main/java/com/example/demo/controller/UserController.java
@@ -7,10 +7,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.common.Result;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
+import lombok.Value;
 import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.websocket.server.PathParam;
 import java.sql.Wrapper;
 
 /**
@@ -32,6 +34,21 @@ public class UserController {
         }
         userMapper.insert(user);
         System.out.println(user);
+        return Result.success();
+    }
+
+    @PutMapping
+    public Result<?> update(@RequestBody User user) {
+        System.out.println("进入了put方法");
+        userMapper.updateById(user);
+        System.out.println(user);
+        return Result.success();
+    }
+
+    @DeleteMapping("/{id}")
+    public Result<?> delete(@PathVariable long id) {
+        System.out.println("进入了delete方法");
+        userMapper.deleteById(id);
         return Result.success();
     }
 
